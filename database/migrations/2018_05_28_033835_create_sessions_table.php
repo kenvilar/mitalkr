ae7cfacf48413c94752826d92db02e58555ef788
Ken Vilar <kenvilar@gmail.com>
2018-05-28 11:47:31 +0800

    Add sessiosn table

---
+++ database/migrations/2018_05_28_033835_create_sessions_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSessionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sessions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('user1_id')->unique();
+	        $table->unsignedInteger('user2_id')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sessions');
+    }
+}
